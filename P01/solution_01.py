57f79fd3ab989c69c5ec45c78e1f9c5334cf820f
kayoung-dev <kayoung150@gmail.com>
2026-01-14 15:40:01 +0900

    Feat: add test cases

--- P01/solution_01.py
+++ P01/solution_01.py
@@ -4,4 +4,7 @@ if data:
     n, k = data[0], data[1]
     weights = data[2:2+n]
     res = [w for w in weights if w >= k]
-    print(f"{len(res)} {sum(res)}")
+    if not res:
+        print("0 0")
+    else:
+        print(f"{len(res)} {sum(res)}")
