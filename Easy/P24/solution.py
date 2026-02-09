f6bd64cc4f28c2d06b5403cf4163986233d85be3
kayoung-dev <kayoungkim150@gmail.com>
2026-02-09 19:29:12 +0900

    Chores: format level naming and markdown

--- Easy/P24/solution.py
+++
@@ -1,9 +0,0 @@
-import sys
-def main():
-    line = sys.stdin.readline().strip()
-    if not line: return
-    try:
-        n, idx = map(int, line.split())
-        print(f"{idx // n} {idx % n}")
-    except: pass
-if __name__ == "__main__": main()
